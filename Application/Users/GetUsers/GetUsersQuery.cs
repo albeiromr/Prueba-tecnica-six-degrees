52f67806efd08ed059214c798cacc6560f93e62a
Albeiro Marín <xlandru@hotmail.com>
2024-03-15 12:04:16 -0500

    Adds the get users query

---
+++ Application/Users/GetUsers/GetUsersQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons.Interfaces;
+using Application.Users.Responses;
+
+namespace Application.Users.GetUsers;
+
+public sealed record GetUsersQuery(
+    Guid rentalId
+) : IQuery<UserResponse>;
---
+++ Application/Users/Responses/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Users.Responses;
+
+/// <summary>
+/// Represents the json response that the user recives
+/// </summary>
+public sealed class UserResponse
+{
+    public int UsuId { get; init; }
+    public string? Nombre { get; init; }
+    public string? Apellido { get; init; }
+}
