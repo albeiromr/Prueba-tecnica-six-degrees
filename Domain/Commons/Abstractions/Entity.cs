f7b0f1c040b145bc40a365809e011b6f6012c252
Albeiro Marín <xlandru@hotmail.com>
2024-03-15 11:24:29 -0500

    Agregar archivos de proyecto.

---
+++ Domain/Commons/Abstractions/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Domain.Commons.Abstractions;
+
+/// <summary>
+/// Represent the base class from where all the 
+/// domain entities inherit
+/// </summary>
+public abstract class Entity
+{
+    public int Id { get; init; }
+
+    /// <summary>
+    /// This default constructor is required for creating entities configurations in the database
+    /// using an ORM.
+    /// </summary>
+    protected Entity() { }
+
+    protected Entity(int id)
+    {
+        Id = id;
+    }
+}
