f2353bad161324debf4dce7901fd84a96af6196a
Albeiro Marín <xlandru@hotmail.com>
2024-03-15 15:57:34 -0500

    Adds de user database configuration

---
+++ Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+/// <summary>
+/// Contains the database table configuration and constraints for the Usuario entity
+/// </summary>
+internal sealed class UserConfiguration : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder.ToTable("Usuario");
+        builder.HasKey(user => user.UsuId);
+        builder.Property(user => user.Nombre);
+        builder.Property(user => user.Apellido);
+    }
+}
